4df41434ac853058c9f9023a463c8d6030b9dc61
zh1peng <frueddy@gmail.com>
2025-04-02 13:59:13 +0800

    cleanup

--- Run.py
+++ Run.py
@@ -4,7 +4,15 @@ subdata = SubjectInformation()
 win, kb, settings = Initialize(subdata)
 trialSeq = GenTrialSeq(settings)
 print(trialSeq.stop)
-ShowInstructions(win)
+intro_test = (
+        'You will perform a stop signal task. \n'
+        'Press "q" for left arrow and "p" for right arrow as fast as possible! \n'
+        'Sometimes the arrow color will turn red. \n'
+        'If that happens, please withhold your response. \n'
+        'Both fast responding and successful stopping are important. \n\n'
+        'Press SPACE to continue.'
+    )
+ShowInstructions(win,intro_text=intro_test)
 CountDown(win)
 RunTask(win, kb, settings, trialSeq, subdata)
 TerminateTask(win)
--- TaskFunc/__init__.py
+++ TaskFunc/__init__.py
@@ -1,10 +1,4 @@
-from .CountDown import CountDown
-from .ShowInstructions import ShowInstructions
-from .SubjectInformation import SubjectInformation
-from .CountDown import RealTimeCountDown
-from .Initialize import Initialize
 from .GenTrialSeq import GenTrialSeq
-from .SetupSeed import SetupSeed
 from .RunTask import RunTask
 from .TerminateTask import TerminateTask
 
