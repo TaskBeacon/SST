cc1c021e170b4dad2cb60eb3a7186441bd20f3b4
zh1peng <frueddy@gmail.com>
2025-05-17 21:32:45 +0800

    cleanup and set trial #

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ from functools import partial
 import serial
 from src import run_trial,Controller, generate_valid_conditions
 
-
-
 # 1. Load config
 cfg = load_config()
 
 # 2. Collect subject info
-subform = SubInfo(cfg['subform'])
+subform = SubInfo(cfg['subform_config'])
 subject_data = subform.collect()
 
 # 3. Load task settings
@@ -21,7 +19,7 @@ settings = TaskSettings.from_dict(cfg['task_config'])
 settings.add_subinfo(subject_data)
 
 # 4. setup triggers
-settings.triggers = cfg['triggers']
+settings.triggers = cfg['trigger_config']
 ser = serial.serial_for_url("loop://", baudrate=115200, timeout=1)
 trigger_sender = TriggerSender(
     trigger_func=lambda code: ser.write([1, 225, 1, 0, (code)]),
@@ -29,7 +27,6 @@ trigger_sender = TriggerSender(
     on_trigger_start=lambda: ser.open() if not ser.is_open else None,
     on_trigger_end=lambda: ser.close()
 )
-
 
 # 5. Set up window & input
 win = Window(size=settings.size, fullscr=settings.fullscreen, screen=1,
